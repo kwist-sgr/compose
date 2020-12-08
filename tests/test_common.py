5662486c6cc59c90e7e7ff8350a3cc97a0ac4a13
Kwist <velnik@gmail.com>
2020-12-08 14:29:52 +0200

    +: common & partial tests

---
+++ tests/test_common.py
@@ -0,0 +1,32 @@
+import compose as cp
+
+
+def test_int():
+    assert isinstance(cp.Int, cp.C)
+    assert cp.Int.func is int
+    assert cp.Int('25') == 25
+
+def test_str():
+    assert isinstance(cp.Str, cp.C)
+    assert cp.Str.func is str
+    assert cp.Str(26) == '26'
+
+def test_set():
+    assert isinstance(cp.Set, cp.C)
+    assert cp.Set.func is set
+    assert cp.Set('251125') == {'1', '2', '5'}
+
+def test_list():
+    assert isinstance(cp.List, cp.C)
+    assert cp.List.func is list
+    assert cp.List('25125') == ['2', '5', '1', '2', '5']
+
+def test_dict():
+    assert isinstance(cp.Dict, cp.C)
+    assert cp.Dict.func is dict
+    assert cp.Dict([('x', 15), ('y', 'Y'), ('z', [1])]) == {'x': 15, 'y': 'Y', 'z': [1]}
+
+def test_sum():
+    assert isinstance(cp.Sum, cp.C)
+    assert cp.Sum.func is sum
+    assert cp.Sum([1, 5, 9, 2]) == 17
---
+++ tests/test_partial.py
@@ -0,0 +1,22 @@
+from unittest.mock import patch
+
+import compose as cp
+
+from .base import sentinel, NamedMock
+
+
+@patch('compose.partial')
+def test_init(mock_partial):
+    func = NamedMock(name='func')
+    v = sentinel.batch('arg1 arg2 kwarg')
+    mock_partial.return_value = func
+    p = cp.P(func, v.arg1, v.arg2, value=v.kwarg)
+    assert p.func is func
+    mock_partial.assert_called_once_with(func, v.arg1, v.arg2, value=v.kwarg)
+
+
+def test_partial():
+    v = [5, 1, 9, 7]
+    p = cp.P(sorted, reverse=True)
+    assert sorted(v) == [1, 5, 7, 9]
+    assert p(v) == [9, 7, 5, 1]
